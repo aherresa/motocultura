6a63e18d4704cbeea118f12d6a8d99c99dd14524
edward ceballos <edwardceballos@edwards-MacBook-Pro.local>
2017-10-18 23:40:51 -0500

    cambios en login

--- src/providers/login-services/login-services.ts
+++ src/providers/login-services/login-services.ts
@@ -2,13 +2,6 @@ import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions } from '@angular/http';
 import 'rxjs/add/operator/map';
 
-
-/*
-  Generated class for the LoginServicesProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class LoginServicesProvider {
 
@@ -17,21 +10,22 @@ export class LoginServicesProvider {
   options: RequestOptions;
 
   constructor(public http: Http) {
-    console.log('Hello LoginServicesProvider Provider');
   }
-  
-  public login (postParams){
-    let body = 'email=' + postParams.email + '&password=' + postParams.password; 
-     
+
+  public login(postParams){
+    let body = 'email=' + postParams.email+'&password=' + postParams.password;
+
     this.headersPost = new Headers({
-      'content-Type':'aplication/x-www-form-urlencoded',
-      'Access-Cotrol-Allow-Origin':'*'
+      'Content-Type':'application/x-www-form-urlencoded',
+      'Access-Control-Allow-Origin':'*'
+    });
+
+    let optionspost = new RequestOptions({
+       headers: this.headersPost
     })
-    let optionspost = new RequestOptions ({
-      headers: this.headersPost
-    }) 
+
     return new Promise ((resolve, reject)=>{
-      this.http.post('https://api-rest-edward.herokuapp.com/api/signin', body, optionspost)
+      this.http.post('https://api-rest-edward.herokuapp.com/api/signin',body,optionspost)
       .subscribe(res=>{
         resolve(res);
       },(err)=>{
